168f48be4f62e74ad1ae04ceb4147cc87ddbddb1
Sarawut <boy_1998_@hotmail.com>
2018-11-27 15:26:14 +0700

    27/11/2561

--- งานชิ้นที่1/Form1.vb
+++ งานชิ้นที่1/Form1.vb
@@ -2,6 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Income.Text = Val(Salary.Text) * Val(12)
         Tax.Text = Val(Income.Text) * Val(0.05)
+
+
         Balance.Text = Val(Income.Text) - Val(Tax.Text)
 
 
